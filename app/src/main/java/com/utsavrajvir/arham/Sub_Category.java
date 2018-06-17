f6bbd8163615f845cf57e5fe50c72375068fc536
Utsav Rajvir <utsavrajvir@gmail.com>
2018-06-17 22:05:14 +0530

    set

---
+++ app/src/main/java/com/utsavrajvir/arham/Sub_Category.java
@@ -0,0 +1,13 @@
+package com.utsavrajvir.arham;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sub_Category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub__category);
+    }
+}
